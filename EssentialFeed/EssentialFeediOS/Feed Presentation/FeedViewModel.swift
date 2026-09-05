6b26c0d4ad207e25b185f156c19bcc233382097f
Mark Kenneth Bayona <mkmbayona@gmail.com>
2026-09-05 23:32:00 +0800

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mark Kenneth Bayona on 9/5/26.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
