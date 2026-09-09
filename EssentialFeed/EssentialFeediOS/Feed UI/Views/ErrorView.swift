42fb20676d48a357ee083111591b88c29da1310c
Mark Kenneth Bayona <mkmbayona@gmail.com>
2026-09-09 20:35:27 +0800

    Move `ErrorView` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeed
+//
+//  Created by Mark Kenneth Bayona on 9/9/26.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
