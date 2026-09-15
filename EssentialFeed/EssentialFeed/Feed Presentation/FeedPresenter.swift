bd825ac3a8a91c64aa250f6e29a7cd3d868e2897
Mark Kenneth Bayona <mkmbayona@gmail.com>
2026-09-15 16:28:58 +0800

    Move `FeedViewModel` to separate file

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -7,10 +7,6 @@
 
 import Foundation
 
-public struct FeedViewModel {
-    public let feed: [FeedImage]
-}
-
 public protocol FeedView: AnyObject {
     func display(_ viewModel: FeedViewModel)
 }
---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Mark Kenneth Bayona on 9/15/26.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
