4b9fc5afdc2331821c0284cc7d0c9fc973f9d43a
Mark Kenneth Bayona <mkmbayona@gmail.com>
2026-09-21 12:43:07 +0800

    Move `CoreDataFeedStore` implementation of the `FeedImageDataStore` protocol to production

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataFeedStore+FeedImageDataLoader.swift
+//  EssentialFeed
+//
+//  Created by Mark Kenneth Bayona on 9/21/26.
+//
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    
+    public func insert(data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+
+    }
+
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(nil))
+    }
+
+}
